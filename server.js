39cb7544a8b84d15b6e0c80e8a8d6442fe34d18d
Petro <larrypetroski@gmail.com>
2023-08-02 15:48:05 -0500

    Trying path join to resolve build issues

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 const dir = '/src/environments';
-const file = 'environment.ts';
-const prodFile = 'environment.prod.ts'
+const file = '/environment.ts';
+const prodFile = '/environment.prod.ts'
 
 const content = `${process.env.FIREBASE_DETAILS}`;
 
@@ -11,14 +11,14 @@ fs.access(dir, fs.constants.F_OK, (err) => {
   if (err) {
     console.log('src does not exist, creating now...', process.cwd());
 
-    fs.mkdir(__dirname + dir, {recursive: true}, (err) => {
+    fs.mkdir(path.resolve(path.join(__dirname, dir)), {recursive: true}, (err) => {
       if (err) throw err;
     });
   }
 
   try {
-    fs.writeFileSync(__dirname + dir + '/' + file, content);
-    fs.writeFileSync(__dirname + dir + '/' + prodFile, content);
+    fs.writeFileSync(path.resolve(path.join(__dirname, dir, file)), content);
+    fs.writeFileSync(path.resolve(path.join(__dirname, dir, prodFile)), content);
     console.log('Created successfully in', process.cwd());
     if (fs.existsSync(dir + '/' + file)) {
       console.log('File is created', path.resolve(dir + '/' + file));
